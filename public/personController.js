1a70f44335cf0e5e4f78c82edce7b8237c07e13e
Dhanashri <dhanashrivilas.ostwal@mavs.uta.edu>
2016-02-12 22:45:06 -0600

    Demo

--- public/personController.js
+++ public/personController.js
@@ -21,7 +21,7 @@ var app = angular.module('myApp', []);
 		console.log($scope.date);
 		var posting = $http({
 			method:'GET',
-			url: '/get',
+			url: '/',
 			data: $scope.data,
 			firstName: $scope.firstName,
 			lastName: $scope.lastName,
