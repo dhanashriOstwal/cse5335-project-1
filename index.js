7a58ad9ab345b201477a3cac35246d700b05ed12
Dhanashri <dhanashrivilas.ostwal@mavs.uta.edu>
2016-02-12 20:18:23 -0600

    Demo

--- index.js
+++ index.js
@@ -18,6 +18,19 @@ app.get('/cool', function(request, response) {
   response.send(cool());
 });
 
+/*to access the posted data from client using request body*/
+app.post('/post', function (req, res) {
+    /* Handling the AngularJS post request*/
+    console.log(req.body);
+    res.setHeader('Content-Type', 'application/json');
+    /*response has to be in the form of a JSON*/
+    req.body.serverMessage = "NodeJS replying to angular"
+        /*adding a new field to send it to the angular Client */
+    res.end(JSON.stringify(req.body));
+    /*Sending the respone back to the angular Client */
+});
+ 
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
