fadbb8b9c8f59eed50316622af28e2a863b76a18
Dhanashri <dhanashrivilas.ostwal@mavs.uta.edu>
2016-02-19 19:13:59 -0600

    Demo

--- public/personController.js
+++ public/personController.js
@@ -9,33 +9,32 @@ app.controller('personCtrl', function($scope) {
  */
 var app = angular.module('myApp', []);
 app.controller('appController', function($scope,$http){
-	$scope.data={}
-	$scope.response={}/* 
-	$scope.firstName="a ";
+	//$scope.data={}
+	//$scope.response={}
+	/* $scope.firstName="a ";
 	$scope.lastName=" b";
 	$scope.date='02/02/2015';
 	$scope.textdata='xx'; */
 	
-	$scope.send = function(){
+	/* $scope.send = function(){
 		console.log("inside click");
 		console.log($scope.data.textdata);
 		console.log($scope.firstName + ' ' + $scope.lastName);
-		console.log($scope.date);
-		var posting = $http({
-			method:'GET',
-			url: '/Name.html',
-			params: {firstname : $scope.firstName, lastName : $scope.lastName, date : $scope.date, txtData : $scope.textdata},
-			data: $scope.data
+		console.log($scope.date);*/
+		/* var posting =  */$http({
+			method:"GET",
+			url: "Name.html"
+			//params: {firstname : $scope.firstName, lastName : $scope.lastName, date : $scope.date, txtData : $scope.textdata},
+			//data: $scope.data
 			//firstName: $scope.firstName,
 			//lastName: $scope.lastName,
 			//date: $scope.date,
 			//processData: false 
-		})
-		posting.then(function (response) {
+		}).then(function mySuccess(response) {
             /*executed when server responds back*/
             //console.log(response);
-            $scope.response.data = response;
-			console.log($scope.response.data);
+            $scope.Message = response.data;
+			//console.log($scope.response.data);
         });
-	}
+	
 });
