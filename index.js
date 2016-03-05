1f426b52eb9e53d78d3a35d7d46e7a0c8471a58a
Dhanashri <dhanashrivilas.ostwal@mavs.uta.edu>
2016-03-05 14:43:14 -0600

    Demo

--- index.js
+++ index.js
@@ -34,21 +34,16 @@ app.post('/post', function (req, res) {
     console.log(req.body);
     res.setHeader('Content-Type', 'application/json');
     /*response has to be in the form of a JSON*/
-	req.body.fname = "Dhanashri";
-	req.body.lname = "Ostwal";
-	req.body.lat = "32.7050";
-	req.body.longitude = "-97.1228";
-	req.body.city = "Arlington";
-	req.body.school = "UTA";
-	req.body.studentId = "1001277328";
-	req.body.schoolOnMap = "UTA is located here!";
-	req.body.gender = "Female";
-	req.body.semester = "Sem 2";
-	req.body.dm = "62";
-	req.body.wdm = "98";
-	req.body.cn = "92";
-	req.body.db = "95";
-	req.body.algo = "70";
+	req.body.student1 = "1001277328,Mary,Smith,Female,University of Texas At Arlington,Sem 2,Arlington,32.7050,-97.1228,UTA is here!,95";
+	req.body.student2 = "1001272218,John,Taylor,Male,Carnegie Mellon University,Sem 4,Pittsburgh,40.4397,-79.9764,CMU is here!,98";
+	req.body.student3 = "4422272218,Ben,George,Male,University of Texas At Dallas,Sem 4,Dallas,32.7767, -96.7970,UTD is here!,56";
+	req.body.student4 = "1001272218,Adam,Mwangi,Male,University of Oklahoma,Sem 4,Norman,35.2200, -97.4400,UO is here!,70";
+	req.body.student5 = "1001272218,Eve,Ann,Female,San Diego University,Sem 4,San Diego,32.7150, -117.1625,SDU is here!,78";
+	req.body.student6 = "1001272218,Ruda,Wilkerson,Female,University of Houston,Sem 2,Houston,29.7604, -95.3698,UH is here!,58";
+	req.body.student7 = "1001272218,Flora,Fernandes,Female,University of Southern California,Sem 3,Los Angeles,34.0500, -118.2500,USC is here!,90";
+	req.body.student8 = "1001272218,Fred,Parker,Male,California State University,Sem 4,Long Beach,33.7683, -118.1956,CSU is here!,80";
+	req.body.student9 = "1001272218,Emma,Henderson,Female,University of Michigan,Sem 2,Ann Arbor,42.2814, -83.7483,UM is here!,77";
+	req.body.student10 = "1001272218,Joseph,Walker,Male,University of Utah,Sem 4,Salt Lake City,40.7500, -111.8833,UU is here!,67";
 	
         /*adding a new field to send it to the angular Client */
     res.end(JSON.stringify(req.body));
--- public/personController.js
+++ public/personController.js
@@ -13,22 +13,34 @@ app.controller('appController', function($scope,$http){
 			$scope.mapShow = true;	
 			$scope.tblShow = true;
 			var jsonString = response.data;
-			$scope.evalled=$scope.$eval(jsonString);
+			//$scope.evalled=$scope.$eval(jsonString);
 			$scope.fromJsoned=angular.fromJson(jsonString);
-			$scope.lat1 = $scope.fromJsoned.lat;
-			$scope.long1 = $scope.fromJsoned.longitude;
-			$scope.infoTitle = $scope.fromJsoned.city;
-			$scope.school = $scope.fromJsoned.school;
-			$scope.details = $scope.fromJsoned.schoolOnMap;
+			
+			$scope.infoTitle = $scope.fromJsoned.city;//6
+			$scope.school = $scope.fromJsoned.school;//4
+			$scope.details = $scope.fromJsoned.schoolOnMap;//9
 			$scope.dm = $scope.fromJsoned.dm;
 			$scope.wdm = $scope.fromJsoned.wdm;
 			$scope.cn = $scope.fromJsoned.cn;
 			$scope.db = $scope.fromJsoned.db;
 			$scope.algo = $scope.fromJsoned.algo;
 			
+			//table
+			$scope.stud1 =  $scope.fromJsoned.student1.split(",");
+			$scope.stud2 = $scope.fromJsoned.student2.split(",");
+			$scope.stud3 = $scope.fromJsoned.student3.split(",");
+			$scope.stud4 = $scope.fromJsoned.student4.split(",");
+			$scope.stud5 = $scope.fromJsoned.student5.split(",");
+			$scope.stud6 = $scope.fromJsoned.student6.split(",");
+			$scope.stud7 = $scope.fromJsoned.student7.split(",");
+			$scope.stud8 = $scope.fromJsoned.student8.split(",");
+			$scope.stud9 = $scope.fromJsoned.student9.split(",");
+			$scope.stud10 = $scope.fromJsoned.student10.split(",");
+			
+			
 			//Google map
 			var mapOptions = {
-				zoom: 10,
+				zoom: 3,
 				center: new google.maps.LatLng(32.7050, -97.1228),
 				mapTypeId: google.maps.MapTypeId.ROADMAP
 			};
@@ -38,16 +50,25 @@ app.controller('appController', function($scope,$http){
 			$scope.markers = [];
     
 			var infoWindow = new google.maps.InfoWindow();
-			var latlng = new google.maps.LatLng($scope.lat1, $scope.long1);
-			//var createMarker = function (){
-        
+			var latlng1 = new google.maps.LatLng($scope.stud1[7], $scope.stud1[8]);
+			var latlng2 = new google.maps.LatLng($scope.stud2[7], $scope.stud2[8]);
+			var latlng3 = new google.maps.LatLng($scope.stud3[7], $scope.stud3[8]);
+			var latlng4 = new google.maps.LatLng($scope.stud4[7], $scope.stud4[8]);
+			var latlng5 = new google.maps.LatLng($scope.stud5[7], $scope.stud5[8]);
+			var latlng6 = new google.maps.LatLng($scope.stud6[7], $scope.stud6[8]);
+			var latlng7 = new google.maps.LatLng($scope.stud7[7], $scope.stud7[8]);
+			var latlng8 = new google.maps.LatLng($scope.stud8[7], $scope.stud8[8]);
+			var latlng9 = new google.maps.LatLng($scope.stud9[7], $scope.stud9[8]);
+			var latlng10 = new google.maps.LatLng($scope.stud10[7], $scope.stud10[8]);
+			
+			var createMarker = function (map, position, title, content, animation){
 				var marker = new google.maps.Marker
 				({
-					map: $scope.map,
-					position: latlng,
-					title: $scope.infoTitle,
-					details: $scope.school + $scope.details,
-					animation:google.maps.Animation.BOUNCE
+					map: map,
+					position: position,
+					title: title,
+					details: content,
+					animation:animation
 				});
 				
 				marker.content = '<div class="infoWindowContent">' + marker.title + '</div>';
@@ -59,12 +80,24 @@ app.controller('appController', function($scope,$http){
 				});
         
 				$scope.markers.push(marker);
-				$scope.map.setCenter(latlng);
+				$scope.map.setCenter(latlng1);
 				$scope.openInfoWindow = function(e, selectedMarker)
 				{
 					e.preventDefault();
 					google.maps.event.trigger(selectedMarker, 'click');
 				}
+			}
+				createMarker($scope.map,latlng1,$scope.stud1[6],$scope.stud1[9],google.maps.Animation.BOUNCE);
+				createMarker($scope.map,latlng2,$scope.stud2[6],$scope.stud2[9],google.maps.Animation.BOUNCE);
+				createMarker($scope.map,latlng3,$scope.stud3[6],$scope.stud3[9],google.maps.Animation.BOUNCE);
+				createMarker($scope.map,latlng4,$scope.stud4[6],$scope.stud4[9],google.maps.Animation.BOUNCE);
+				createMarker($scope.map,latlng5,$scope.stud5[6],$scope.stud5[9],google.maps.Animation.BOUNCE);
+				createMarker($scope.map,latlng6,$scope.stud6[6],$scope.stud6[9],google.maps.Animation.BOUNCE);
+				createMarker($scope.map,latlng7,$scope.stud7[6],$scope.stud7[9],google.maps.Animation.BOUNCE);
+				createMarker($scope.map,latlng8,$scope.stud8[6],$scope.stud8[9],google.maps.Animation.BOUNCE);
+				createMarker($scope.map,latlng9,$scope.stud9[6],$scope.stud9[9],google.maps.Animation.BOUNCE);
+				createMarker($scope.map,latlng10,$scope.stud10[6],$scope.stud10[9],google.maps.Animation.BOUNCE);
+				
 				
 				
 				//Pie chart
@@ -77,20 +110,25 @@ app.controller('appController', function($scope,$http){
 				google.charts.setOnLoadCallback(drawChart);
 				function drawChart() {
 					var data = google.visualization.arrayToDataTable([
-						['Subjects', 'Marks'],
-						['Data Mining', dataMining],
-						['Web Data Management', webData],
-						['Computer Networks', compNetworks],
-						['Database Systems', databaseSystems], 
-						['Algorithms', algorithms]
+						['Students', 'Marks'],
+						[$scope.stud1[1], parseInt($scope.stud1[10])],
+						[$scope.stud2[1], parseInt($scope.stud2[10])],
+						[$scope.stud3[1], parseInt($scope.stud3[10])],
+						[$scope.stud4[1], parseInt($scope.stud4[10])], 
+						[$scope.stud5[1], parseInt($scope.stud5[10])],
+						[$scope.stud6[1], parseInt($scope.stud6[10])],
+						[$scope.stud7[1], parseInt($scope.stud7[10])],
+						[$scope.stud8[1], parseInt($scope.stud8[10])],
+						[$scope.stud9[1], parseInt($scope.stud9[10])],
+						[$scope.stud10[1], parseInt($scope.stud10[10])]
 					]);
 
 					var options = {
-						title: 'Semester I - Marks',
+						title: 'Semester I - Average Marks',
 						is3D: true,
 					};
 
-					var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+					var chart = new google.visualization.BarChart(document.getElementById('piechart_3d'));
 					chart.draw(data, options);
 				}
         });
